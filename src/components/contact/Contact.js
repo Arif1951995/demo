b0b0b54431e17403a39f9ac35e0468601606d062
Arif1951995 <40889693+Arif1951995@users.noreply.github.com>
2019-09-22 05:19:18 -0700

    added contact section and footer

---
+++ src/components/contact/Contact.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Contact.css'
+
+
+const Contact = () => {
+    return (
+        <section id="contact">
+        <h2>Contact Us</h2>
+        <div class="icons">
+                <a href="#"><i class="fab fa-twitter"></i></a>
+                <a href="#"><i class="fab fa-facebook-square"></i></a>
+                <a href="#"><i class="fab fa-youtube"></i></a>
+        </div>
+
+    </section>
+    );
+}
+
+export default Contact
